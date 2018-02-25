44e0404a910c0bbc19622f5238676da7e798936d
walison filipe <walisonfilipe@hotmail.com>
2018-02-25 10:40:41 -0300

    Remove cobertura do wsgi no sentry

--- pulso/wsgi.py
+++ pulso/wsgi.py
@@ -1,9 +1,7 @@
 import os
 
-from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 from django.core.wsgi import get_wsgi_application
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pulso.settings")
-
-application = Sentry(get_wsgi_application())
+application = get_wsgi_application()
