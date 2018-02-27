9ba32866d7865ff04f9605d218440b969b556af1
walison filipe <walisonfilipe@hotmail.com>
2018-02-27 12:18:21 -0300

    Altera tipo de mensagem do fcm e adiciona helpers para relacionamento

--- notifications/signals.py
+++ notifications/signals.py
@@ -1,18 +1,16 @@
 from django.dispatch import receiver, Signal
-from django.urls import reverse_lazy
 
 from fcm_django.models import FCMDevice
 
 user_was_followed = Signal(providing_args=['followee', 'follower'])
 
+
 @receiver(user_was_followed)
 def notify_new_follower(sender, follower, followee, **kwargs):
     devices = FCMDevice.objects.filter(user=followee)
     if devices.exists():
-        data_message = {
-            'title': f'{follower.name} seguiu você',
+        extra = {
             'notification_type': 'follower',
             'follower_id': follower.id
         }
-        devices.send_message(data=data_message)
-        
+        devices.send_message(title=f'{follower.name} seguiu você', data=extra)
---
+++ relationships/helpers.py
@@ -0,0 +1,12 @@
+from notifications.signals import user_was_followed
+
+from .models import Follow
+
+
+def follow(from_user, to_user):
+    user_was_followed.send(follower=from_user, followee=to_user)
+    Follow.objects.create(from_user=from_user, to_user=to_user)
+
+
+def unfollow(from_user, to_user):
+    Follow.objects.filter(from_user=from_user, to_user=to_user).delete()
